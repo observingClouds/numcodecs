af8cf12fcd791445f949909d4bddb08841033e4c
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-03-29 17:07:57 +0200

    prevent overwriting input buffer

--- numcodecs/bitround.py
+++ numcodecs/bitround.py
@@ -16,12 +16,9 @@ class BitRound(Codec):
     def encode(self, buf):
         if self.keepbits == 23:
             return buf
-        # TODO: figure out if we need to make a copy
-        # Currently this appears to be overwriting the input buffer
-        # Is that the right behavior?
         a = ensure_ndarray(buf).view()
         assert a.dtype == np.float32
-        b = a.view(dtype=np.int32)
+        b = a.view(dtype=np.int32).copy()
         maskbits = 23 - self.keepbits
         mask = (0xFFFFFFFF >> maskbits) << maskbits
         half_quantum1 = (1 << (maskbits - 1)) - 1
